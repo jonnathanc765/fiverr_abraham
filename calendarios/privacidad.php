1019e7eba814d668debb4f21a573088a3d557860
Jonnathan Carrasco <jonnathan.c.765@gmail.com>
2019-07-15 21:05:22 -0400

    vista de privacidad arreglada

--- calendarios/privacidad.php
+++ calendarios/privacidad.php
@@ -1,33 +1,52 @@
 <?php
-include('includes/global.php');
+// include('includes/global.php');
 ?>
 <style>
 
+	
 .div_container_ficha2{
-border:0px solid red;
-/*width:530px;*/
-width:670px;
-height:440px;
-overflow:auto;
-position:relative;
-/*left:30px;*/
-padding-left:30px;
-padding-right:30px;
-font-family:"Trebuchet MS" ,Arial, Helvetica, sans-serif;
-font-size:11px;
-line-height:20px;
+	border:0px solid red;
+	/*width:530px;*/
+	width:670px;
+	height:440px;
+	overflow:auto;
+	position:relative;
+	/*left:30px;*/
+	margin: 10px auto;
+	color: #fff;
+	font-family:"Trebuchet MS" ,Arial, Helvetica, sans-serif;
+	font-size:11px;
+	line-height:20px;
 }
 .titulo_producto{
-font-family:"Trebuchet MS" ,Arial, Helvetica, sans-serif;
-font-size:14px;
-color:#000000;
-font-weight:bold;
-padding-bottom:10px;
+	font-family:"Trebuchet MS" ,Arial, Helvetica, sans-serif;
+	font-size:14px;
+	color:#fff;
+	font-weight:bold;
+	padding-bottom:10px;
 }
-.clear { clear: both; }
+.clear { 
+	clear: both; 
+}
 
-.volver_privacidad a{ display:block; width:80px; height:20px; color:#000000; text-decoration:none; font-size:18px; border:1px solid #666666;}
-.volver_privacidad a:hover{ display:block; width:80px; height:20px; color:#ffa700; font-size:18px; text-decoration:none; border:1px solid #ffa700;}
+.volver_privacidad a { 
+	display: block; 
+	width: 80px; 
+	height: 20px; 
+	color: #000000;
+	text-decoration: none; 
+	font-size: 18px; 
+	border: 1px solid #666666;
+}
+.volver_privacidad a:hover { 
+	display:block; 
+	width:80px; 
+	height:20px; 
+	color:#ffa700; 
+	font-size:18px; 
+	text-decoration:none; 
+	border:1px solid #ffa700;
+	}
 
 </style>
 
@@ -38,13 +57,13 @@ padding-bottom:10px;
 	<!--<div class="div_perdida">-->
 		<div class="titulo_producto"><?=$msg[13][1]?></div>
 		<div>&nbsp;</div>
-		<?=$msg[13][2]?>
+		<div class="privacy-container">
+			<?=$msg[13][2]?>
+		</div>
 		<div>&nbsp;</div>
 		<div>&nbsp;</div>
-
+		<div class="clear"></div>
 		
-
-	    <div class="clear"></div>
 </div>
 
 <div class="clear"></div>
